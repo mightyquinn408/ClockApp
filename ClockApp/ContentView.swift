2bad10cb4fbf600c354c2427137809e38efa54eb
Mike Quinn <mike.quinn2@workday.com>
2022-12-14 15:34:13 -0700

    Project setup, added initial clock view and angles

--- ClockApp/ContentView.swift
+++ ClockApp/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TimelineView(.animation) { timeline in
+            Canvas { context, size in
+                let clock = ClockAngles(for: timeline.date)
+                
+                let drawRect = CGRect(origin: .zero, size: size)
+                let radius = min(size.width, size.height) / 2
+                
+                let borderThickness = radius / 25
+                let innerBlackRingSize = radius / 6
+                let centerSize = radius / 40
+                
+                let hourHandLength = radius / 2.5
+                let minuteHandLength = radius / 1.5
+                
+                let seconHandLength = radius * 1.1
+                let secondHandWidth = radius / 25
+                
+                context.stroke(Circle().inset(by: borderThickness / 2).path(in: drawRect), with: .color(.primary), lineWidth: borderThickness)
+                context.translateBy(x: drawRect.midX, y: drawRect.midY)
+            }
         }
-        .padding()
     }
 }
 

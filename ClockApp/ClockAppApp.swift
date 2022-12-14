3c8b833d70185c3ca0f8481b7963b02212e14f1a
Mike Quinn <mike.quinn2@workday.com>
2022-12-14 08:56:55 -0700

    Initial Commit

---
+++ ClockApp/ClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockAppApp.swift
+//  ClockApp
+//
+//  Created by Mike Quinn on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
